22b2947bc0578e32ccef937eb8269fb4a31cb218
violetttt <iniiuwu@gmail.com>
2026-05-05 01:32:50 +0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,46 @@
+const sections = document.querySelectorAll(".recipe");
+const navLinks = document.querySelectorAll("nav a");
+
+window.addEventListener("scroll", () => {
+    let current = "";
+
+    sections.forEach(section => {
+        const sectionTop = section.offsetTop - 100;
+        if (scrollY >= sectionTop) {
+            current = section.getAttribute("id");
+        }
+    });
+
+    navLinks.forEach(link => {
+        link.classList.remove("active");
+        if (link.getAttribute("href") === "#" + current) {
+            link.classList.add("active");
+        }
+    });
+});
+
+
+const observer = new IntersectionObserver(entries => {
+    entries.forEach(entry => {
+        if (entry.isIntersecting) {
+            entry.target.classList.add("show");
+        }
+    });
+});
+
+document.querySelectorAll(".recipe").forEach(el => {
+    el.classList.add("hidden");
+    observer.observe(el);
+});
+
+
+
+document.querySelectorAll("nav a").forEach(btn => {
+    btn.addEventListener("mousedown", () => {
+        btn.style.transform = "scale(0.95)";
+    });
+
+    btn.addEventListener("mouseup", () => {
+        btn.style.transform = "";
+    });
+});
